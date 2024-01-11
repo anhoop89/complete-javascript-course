7fd4e7e8d22523367762a9d3ba65277ac53a43cf
Anh Ho <anhho@pdx.edu>
2024-01-11 09:37:32 -0800

    testing with show modal 123 in a loop

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModal);
+
+for (const x in btnOpenModal) {
+    btnOpenModal[x].addEventListener('click', function () {
+        console.log('Button clicked: ', btnOpenModal[x].textContent);
+    });
+}
+
