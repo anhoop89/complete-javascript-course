dff033fcdb538c038fd993f102752ab24aa96a86
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-04-27 11:20:13 -0700

    update the function fetching api

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,7 +3,21 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-
+const renderCountry = (data) => {
+  const html = `
+   <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span> ${(data.population / 1000000).toFixed(1)} mil people</p>
+        ${data.languages && data.languages.length > 0 ? `<p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>` : ''}
+        ${data.currencies && data.currencies.length > 0 ? `<p class="country__row"><span>💰</span>${data.currencies[0].name}</p>` : ''}
+      </div>
+    </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html)
+  countriesContainer.style.opacity = 1;
+}
 
 ///////////////////////////////////////
 const getCountryData = function (country) {
@@ -15,19 +29,7 @@ request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText)
     console.log(data);
     console.log(data.flag);
-    const html = `
-   <article class="country">
-      <img class="country__img" src="${data.flag}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span> ${(data.population / 1000000).toFixed(1)} mil people</p>
-        ${data.languages && data.languages.length > 0 ? `<p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>` : ''}
-        ${data.currencies && data.currencies.length > 0 ? `<p class="country__row"><span>💰</span>${data.currencies[0].name}</p>` : ''}
-      </div>
-    </article>`;
-    countriesContainer.insertAdjacentHTML('beforeend', html)
-    countriesContainer.style.opacity = 1;
+    renderCountry(data)
 })
 
 }
